3a200cf3be5c5c04af76d02314693ab7e65af31c
tangbaobao <tangbaoabo@nefu.edu.cn>
2018-04-01 12:03:55 +0800

    2013

--- src/main/java/com/tangbaobao/controller/welcome.java
+++ src/main/java/com/tangbaobao/controller/welcome.java
@@ -8,5 +8,6 @@ package com.tangbaobao.controller;
 public class welcome {
     public static void main(String[] args) {
         System.out.println("你好");
+        System.out.println("hahah");
     }
 }
